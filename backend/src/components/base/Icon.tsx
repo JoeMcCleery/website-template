7b05d7c7df1bda545cb2ffd44fd04e8323da643e
JoeMcCleery <mccleery.joseph@gmail.com>
2025-01-18 21:51:13 +0800

    Update backend base/icon

--- backend/src/components/base/Icon.tsx
+++ backend/src/components/base/Icon.tsx
@@ -1,3 +1,11 @@
-export default function Icon({ name }: { name?: string }) {
-  return <span className="material-symbols-rounded">{name}</span>
+import clsx from 'clsx'
+
+export default function Icon({ name, fill }: { name?: string; fill?: boolean }) {
+  const classNames = clsx('material-symbols-rounded', { fill: fill })
+
+  return (
+    <div className="icon-fill-hover">
+      <span className={classNames}>{name}</span>
+    </div>
+  )
 }
