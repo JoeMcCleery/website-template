af07035236f68e5b74af118ca7b1cfeb55a934e3
JoeMcCleery <mccleery.joseph@gmail.com>
2025-02-01 14:03:17 +0800

    Use custom favicon in app.vue

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import type { Media } from '@common/payload-types'
+
+const { data: siteConfig } = useGlobal('site-config')
+
+const favicon = computed(() => (siteConfig.value?.settings?.favicon as Media)?.url)
+useFavicon(favicon)
+</script>
